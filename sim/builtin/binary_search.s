bb28fac02a173478706b8c783464427e072d3799
oneblueandred <nolanbailey21@gmail.com>
2025-09-08 01:46:58 -0700

    Update binary_search.s

--- sim/builtin/binary_search.s
+++ sim/builtin/binary_search.s
@@ -28,15 +28,13 @@ add a0, x0, t2
 jalr x0, ra, 0
 
 NOT_FOUND:
-
 blt a2, a1, INCORRECT_BOUNDS # if (r < l) return -1.
 
 sub t0, a2, a1 # store r-l.
 srai t1, t0, 1 # store (r-l)/2.
 add t2, t1, a1 # store l + (r-l)/2.
 
-# Get the 'mid' element in 'arr'.
-slli t3, t2, 2 # mid * sizeof(int)
+slli t3, t2, 2 # mid * sizeof(int) # Get the 'mid' element in 'arr'.
 add t3, t3, a0 # pointer to arr[mid].
 lw t0, 0(t3)   # get value at arr[mid].
 
@@ -45,7 +43,6 @@ add a0, x0, t2
 jalr x0, ra, 0
 
 NOT_EQ:
-
 bge a3, t0, GREATER_THAN # if (x > arr[mid]) return bin_search(arr, mid+1, r, x).
 addi a2, t2, -1 # r = mid - 1 # bin_search(arr, l, mid-1, x);
 jal BIN_SEARCH
